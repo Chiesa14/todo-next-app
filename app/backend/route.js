73116158794d5517e6b95f543aa04679aac489c9
Chiesa14 <remychiesa14@gmail.com>
2024-02-21 07:10:13 +0200

    handling Post

--- app/backend/route.js
+++ app/backend/route.js
@@ -1,4 +1,5 @@
 import { ConnectDB } from "@/lib/config/db";
+import TodoModel from "@/lib/models/Todo";
 import { NextResponse } from "next/server";
 
 const LoadDB = async () => {
@@ -7,5 +8,15 @@ const LoadDB = async () => {
 LoadDB();
 
 export async function GET(request) {
-  return NextResponse.json({ msg: "Response from server" });
+  return NextResponse.json({ msg: "Get method hit" });
+}
+
+export async function POST(request) {
+  const { title, description } = await request.json();
+  const newTodo = new TodoModel({
+    title: title,
+    description: description,
+  });
+  newTodo.save();
+  return NextResponse.json({ msg: "Post method hit" });
 }
