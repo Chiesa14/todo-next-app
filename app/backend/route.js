5ee0c44a5342af18eda5cea84205c4ed6192bb80
Chiesa14 <remychiesa14@gmail.com>
2024-02-21 15:21:40 +0200

    delete request

--- app/backend/route.js
+++ app/backend/route.js
@@ -8,16 +8,20 @@ const LoadDB = async () => {
 LoadDB();
 
 export async function GET(request) {
-  const todos = await TodoModel.find({});
+  const todos = await TodoModel.find().sort({ updatedAt: -1 });
   return NextResponse.json({ todos: todos });
 }
 
 export async function POST(request) {
   const { title, description } = await request.json();
-  await TodoModel({
+  await TodoModel.create({
     title: title,
     description: description,
   });
-  newTodo.save();
   return NextResponse.json({ msg: "Added Successfully" });
+}
+export async function DELETE(request) {
+  const id = await request.nextUrl.searchParams.get("mongoID");
+  await TodoModel.findByIdAndDelete(id);
+  return NextResponse.json({ msg: "Deleted Successfully" });
 }
